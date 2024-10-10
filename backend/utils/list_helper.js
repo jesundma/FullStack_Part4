7d18338e92a96067e7f784a1ee77b488af929eee
jesundma <jyrki.sundman12@gmail.com>
2024-10-10 13:11:03 +0300

    Task 4.4 ready

--- backend/utils/list_helper.js
+++ backend/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.lenght === 0) {
+   return 0
+  }
+
+  let result = 0
+  blogs.forEach(blog => {
+    result += blog.likes
+  })
+  return result
+}
+
+
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
