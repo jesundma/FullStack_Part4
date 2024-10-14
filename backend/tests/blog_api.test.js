bfd06e571d1311caea6a786ab354127893295fbd
jesundma <jyrki.sundman12@gmail.com>
2024-10-14 14:09:30 +0300

    Defining db for test cases ready.

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -1,10 +1,20 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const assert = require('node:assert')
+const helper = require('./test_helper')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
 
 const api = supertest(app)
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  const blogObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = blogObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
 
 test('notes are returned as json', async () => {
   const response = await api
@@ -15,7 +25,7 @@ test('notes are returned as json', async () => {
 
 test('there are two notes', async () => {
   const response = await api.get('/api/blogs')  
-    assert.strictEqual(response.body.length, 2)
+    assert.strictEqual(response.body.length, 6)
   })
   
   after(async () => {
