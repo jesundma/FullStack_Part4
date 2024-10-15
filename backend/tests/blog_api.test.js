73f047a01867e7e3ac6b6558cd33b0b3a1063d57
jesundma <jyrki.sundman12@gmail.com>
2024-10-15 12:51:12 +0300

    Added eslint config

--- backend/tests/blog_api.test.js
+++ backend/tests/blog_api.test.js
@@ -42,6 +42,44 @@ test('blog identifiers are in id field, not _id', async () => {
     })
   })
 
+test('add new blog functions and confirm that original and response values are same', async () => {
+  const newBlog = {
+    "title": "Meaning of life",
+    "author": "Douglas, A.",
+    "url": "http://douglasadams.com",
+    "likes": 42
+  }
+    
+  const postedBlog = await api
+    .post('/api/blogs/')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+
+  const { id, ...postedBlogDropId } = postedBlog.body
+
+  assert.strictEqual(response.body.length, helper.initialBlogs.length + 1)
+  assert.deepStrictEqual(postedBlogDropId, {
+    title: newBlog.title,
+    author: newBlog.author,
+    url: newBlog.url,
+    likes: newBlog.likes
+  })
+test('field likes has default value of 0 if no value provided', async () => {
+  const newBlog = {
+    "title": "Spice Must Flow",
+    "author": "Spacing Guild",
+    "url": "http://melange.com",
+    "likes": 
+  }
+
+  
+  })
+})
+
+
   after(async () => {
     await mongoose.connection.close()
   })
