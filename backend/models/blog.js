4abf985ee2b930e7c1b10e902ae4f1bc3c92e6bd
jesundma <jyrki.sundman12@gmail.com>
2024-11-17 20:33:55 +0200

    blogSchema changed to flat schema. Content: { } would require changes to routes.

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose')
 
 const blogSchema = mongoose.Schema({
-  content: {
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  likes: {
+    type: Number,
+    default: 0
   },
   user: {
     type: mongoose.Schema.Types.ObjectId,
