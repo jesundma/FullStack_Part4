6d3a8eed2f9cd17a421ddbfef61a1f30fa781e1a
jesundma <jyrki.sundman12@gmail.com>
2024-12-01 11:32:55 +0200

    Users controller error request.body -> body.xx to request.body -> username / password.

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -11,13 +11,13 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
   const { username, name, password } = request.body
 
-  if (!body.username || body.username.length < 3) {
+  if (!username || username.length < 3) {
     return response.status(400).json({
       error: 'Username is required and must be at least 3 characters long'
     })
   }
 
-  if (!body.password || body.password.length < 3) {
+  if (!password || password.length < 3) {
     return response.status(400).json({
       error: 'Password is required and must be at least 3 characters long'
     })
