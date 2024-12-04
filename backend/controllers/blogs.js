0523e93a9bf589d0c6d8656dbb43dc78fdfcb4b6
jesundma <jyrki.sundman12@gmail.com>
2024-12-04 21:39:18 +0200

    Task 4.17 -.19 ready. Api/blogs and /users response in correct format. Token login functions. POST functions only with valid token.

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -3,37 +3,52 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
     .find({}).populate('user', { username: 1, name: 1 })
-
   response.json(blogs)
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
 blogsRouter.post('/', async (request, response) => {
+  
   const body = request.body
-  console.log('Request body:', body)
-
-  if (!body.title || !body.url) {
-    return response.status(400).json({
-      error: 'Title and URL required'
-    })
+ 
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
   }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
+  if (!decodedToken.id || !body.title || !body.url) {
+    const errorMessage = !decodedToken.id
+      ? 'Token invalid'
+      : 'Title and URL required';
+  
+    return response.status(!decodedToken.id ? 401 : 400).json({ error: errorMessage });
+  }
+  
   const blog = new Blog({
     title: body.title,
     author: body.author,
